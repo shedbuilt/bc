9eddf1553ff6bacfa0eb371a34ac0a89a9635380
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:22:34 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Change an internal script to use sed rather than ed
-cp "${SHED_PATCHDIR}/fix-libmath_h" "${SHED_SRCDIR}/bc/"
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+cp "${SHED_PKG_PATCH_DIR}/fix-libmath_h" "${SHED_PKG_SOURCE_DIR}/bc/"
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     # Establish temporary symlinks so bc will see ncurses during configuration
     ln -sv /tools/lib/libncursesw.so.6 /usr/lib/libncursesw.so.6
     ln -sfv libncurses.so.6 /usr/lib/libncurses.so
@@ -11,9 +11,9 @@ fi
             --with-readline         \
             --mandir=/usr/share/man \
             --infodir=/usr/share/info || exit 1
-make -j $SHED_NUMJOBS || exit 1
-make "DESTDIR=$SHED_FAKEROOT" install || exit 1
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+make -j $SHED_NUM_JOBS || exit 1
+make "DESTDIR=$SHED_FAKE_ROOT" install || exit 1
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
     # Remove temporary symlinks
     rm -v /usr/lib/libncurses.so
     rm -v /usr/lib/libncursesw.so.6
