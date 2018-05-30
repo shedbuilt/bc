f9a76ebdd589b2d4161d95a0c0ca612c40cb23a9
Auston Stewart <auston@shedbuilt.net>
2018-05-29 18:08:29 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 # Change an internal script to use sed rather than ed
-cp "${SHED_PKG_PATCH_DIR}/fix-libmath_h" "${SHED_PKG_SOURCE_DIR}/bc/"
-if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
     # Establish temporary symlinks so bc will see ncurses during configuration
-    ln -sv /tools/lib/libncursesw.so.6 /usr/lib/libncursesw.so.6
-    ln -sfv libncurses.so.6 /usr/lib/libncurses.so
-    sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure
+    ln -sv /tools/lib/libncursesw.so.6 /usr/lib/libncursesw.so.6 &&
+    ln -sfv libncurses.so.6 /usr/lib/libncurses.so &&
+    sed -i -e '/flex/s/as_fn_error/: ;; # &/' configure || exit 1
 fi
+cp "${SHED_PKG_CONTRIB_DIR}/fix-libmath_h" "${SHED_PKG_SOURCE_DIR}/bc/" &&
 ./configure --prefix=/usr           \
             --with-readline         \
             --mandir=/usr/share/man \
-            --infodir=/usr/share/info || exit 1
-make -j $SHED_NUM_JOBS || exit 1
+            --infodir=/usr/share/info &&
+make -j $SHED_NUM_JOBS &&
 make "DESTDIR=$SHED_FAKE_ROOT" install || exit 1
-if [ "$SHED_BUILD_MODE" == 'bootstrap' ]; then
+
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
     # Remove temporary symlinks
     rm -v /usr/lib/libncurses.so
     rm -v /usr/lib/libncursesw.so.6
